bef6b646af4d37e80bc2c1cb9bb48789b62dbb6a
jberkovica <jberkovica@evolutiongaming.com>
2020-12-13 22:58:02 +0200

    improvements after review

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const si = require("systeminformation");
 const express = require("express");
 const http = require("http");
 const io = require("socket.io")(http);
+const path = require("path");
 
 // setting server
 const app = express();
@@ -11,22 +12,37 @@ io.listen(server);
 // configuration
 const refreshInterval = 3000;
 const periodSeconds = 60;
-const tempInPeriod = [];
 const periodMaxLen = (periodSeconds * 1000) / refreshInterval;
 
-// accessing static css file
-app.use(express.static(__dirname + "/public"));
+// TODO: generic - rewrite using React
 
+// accessing static css file
+// TODO: use Path.join so it is working on all OS
+app.use(express.static(path.join(__dirname, "/public")));
+
+// TODO: use Path.join so it is working on all OS
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+    res.sendFile(path.join(__dirname, "/index.html"));
 });
 
 io.on("connection", (socket) => {
+    // TODO: take out setInterval?
+    // problem:
+    // what happens when we have multiple connections and we manipulate same tempInPeriod
+    // define tempInPerios inside
+
+    console.log("New connection: " + socket.id);
+    const tempInPeriod = [];
+
+    monitorCPU(socket, tempInPeriod);
+});
+
+function monitorCPU(socket, tempInPeriod) {
     setInterval(function () {
         si.cpuTemperature()
             .then(function (data) {
-                const tempNow = data.main.toFixed(1);
-                const tempAvg = calcAverage(data.main).toFixed(1);
+                const tempNow = parseFloat(data.main.toFixed(1));
+                const tempAvg = parseFloat(calcAverage(tempNow, tempInPeriod).toFixed(1));
 
                 console.log("Current temp: " + tempNow);
                 console.log("Last min average: " + tempAvg);
@@ -35,23 +51,25 @@ io.on("connection", (socket) => {
             })
             .catch((error) => console.error(error));
     }, refreshInterval);
-});
+}
 
-function calcAverage(temp) {
-    // calculate average for given period
-    if (tempInPeriod.length <= periodMaxLen) {
-        tempInPeriod.push(temp);
-    } else {
+function calcAverage(temp, tempInPeriod) {
+    tempInPeriod.push(temp);
+    // TODO: use splice instead of shift?
+    if (tempInPeriod.length > periodMaxLen) {
         tempInPeriod.shift();
-        tempInPeriod.push(temp);
     }
 
+    // TODO: come up with more elegant solution
+    // var1
+    // const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    // let total = tempInPeriod.reduce(reducer);
+
+    // var2
     let total = 0;
-    for (let i = 0; i < tempInPeriod.length; i++) {
-        total += tempInPeriod[i];
-    }
-    const avg = total / tempInPeriod.length;
-    return avg;
+    tempInPeriod.forEach((element) => (total += element));
+
+    return total / tempInPeriod.length;
 }
 
 server.listen(3000, () => {
